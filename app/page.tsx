f7646070f2d89ce52e6b24b7c4d70017875a817d
DobondiUVT <laszlo.dobondi02@e-uvt.ro>
2023-07-31 19:37:05 +0300

    prisma integration, mock schema

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Image from 'next/image'
+import { PrismaClient } from '@prisma/client'
 
-export default function Home() {
+
+
+export default async function Home() {
+
+  const prisma = new PrismaClient()
+  const allNotes = await prisma.note.findMany()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -8,6 +14,13 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
+        <div>
+          {allNotes.map((note) => (
+            <div key={note.id}>
+              <h1>{note.title}</h1>
+              <p>{note.content}</p>
+            </div>))}
+        </div>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
