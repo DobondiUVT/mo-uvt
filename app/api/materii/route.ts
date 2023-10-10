4c38f2347f4354d042374dd7d0ea7d734468127d
DobondiUVT <laszlo.dobondi02@e-uvt.ro>
2023-10-11 02:14:45 +0300

    cache store fix

--- app/api/materii/route.ts
+++ app/api/materii/route.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 
+export const revalidate = 0;
+
 export async function GET(request: Request) {
   const prisma = new PrismaClient();
   const materii = await prisma.materie.findMany()
